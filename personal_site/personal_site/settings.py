b7dd6dbb19e54ce760f15960f0d12bd7663505d0
Sibusiso Mathebula <sibusisomathebula041@gmail.com>
2026-05-09 19:56:48 +0200

    Update settings.py

--- personal_site/personal_site/settings.py
+++ personal_site/personal_site/settings.py
@@ -122,7 +122,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = '/static/'
+STATIC_URL = 'blog/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
