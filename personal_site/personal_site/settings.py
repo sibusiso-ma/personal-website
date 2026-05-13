2142a7a13f1950b89b668f5c2a75be055532acf8
Sibusiso Mathebula <sibusisomathebula041@gmail.com>
2026-05-13 11:58:23 +0200

    Update settings.py

--- personal_site/personal_site/settings.py
+++ personal_site/personal_site/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'personal_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
