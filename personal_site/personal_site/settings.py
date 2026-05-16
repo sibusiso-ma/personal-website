e405de3a3ce3dc798394126aba9ca3273ba44788
Sibusiso Mathebula <sibusisomathebula041@gmail.com>
2026-05-16 11:54:12 +0200

    Change STATIC_URL to use root path

--- personal_site/personal_site/settings.py
+++ personal_site/personal_site/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = 'blog/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
